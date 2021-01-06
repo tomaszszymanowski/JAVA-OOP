f9001aba601fc0372c6608159ad8c609851c7810
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2021-01-06 20:30:25 +0100

    #1 program do wyświetlania informacji o fakturach

---
+++ src/invoiceProgram/Invoice.java
@@ -0,0 +1,15 @@
+package invoiceProgram;
+
+public class Invoice {
+
+    int invNumber = 0;
+    char[] description ;
+    long price = 0;
+    double vatRate = 0;
+    int quantity = 0;
+    char[] Buyer;
+    int buyerNIP = 0;
+    char[] Seller;
+    int sellerNIP = 0;
+
+}
