b8dea1b4eef2c84d7ffd744ee9f8a5bef37e5a1a
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2021-01-07 20:05:22 +0100

    #11 zadadnie 3 program dla serwisu samochodowego

---
+++ src/carService/Car.java
@@ -0,0 +1,18 @@
+package carService;
+
+public class Car {
+    String mark;
+    String model;
+    int productionDate;
+    String faultDescription;
+
+
+    Car(String mark, String model, int productionDate, String faultDescription){
+        this.mark = mark;
+        this.model = model;
+        this.productionDate = productionDate;
+        this.faultDescription = faultDescription;
+
+    }
+
+}
