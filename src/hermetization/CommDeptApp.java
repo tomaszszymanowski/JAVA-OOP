fa3586340c32ed897953a7e47fd164e147a1ffff
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2021-02-06 15:17:41 +0100

    #31 Zadanie 3 - menu programu z obsługą błędu złego wyboru opcji w menu

--- src/hermetization/CommDeptApp.java
+++ src/hermetization/CommDeptApp.java
@@ -9,22 +9,34 @@ public class CommDeptApp {
         int choose;
 
         do{
+
             System.out.println("\n----- Communication Department - Customer Service application -----");
             System.out.println("select the operation number\n");
             System.out.println("1 - submission of the application ");
             System.out.println("2 - change submission status ");
             System.out.println("3 - checking the status of the application ");
             System.out.println("100 - end program ");
+
+
+            while(!scanner.hasNextInt()) {
+                scanner.next();
+                System.out.println("To nie jest poprawna opcja");
+            }
             choose = scanner.nextInt();
+
 
             switch (choose){
                 case 1:
+                    System.out.println("*****");
+
                     System.out.println("Wniosek złożony");
                     break;
                 case 2:
+                    System.out.println("*****");
                     System.out.println("Zmienił się status wniosku");
                     break;
                 case 3:
+                    System.out.println("*****");
                     System.out.println("wyświetla status wniosku");
                     break;
                 case 100:
