fbb3884459bc6fd214bdb63a23a64503b669508c
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2021-02-06 14:32:02 +0100

    #31 Zadanie 3 - przygotowanie menu programu

---
+++ src/hermetization/CommDeptApp.java
@@ -0,0 +1,37 @@
+package hermetization;
+
+import java.util.Scanner;
+
+public class CommDeptApp {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        int choose;
+
+        do{
+            System.out.println("\n----- Communication Department - Customer Service application -----");
+            System.out.println("select the operation number\n");
+            System.out.println("1 - submission of the application ");
+            System.out.println("2 - change submission status ");
+            System.out.println("3 - checking the status of the application ");
+            System.out.println("100 - end program ");
+            choose = scanner.nextInt();
+
+            switch (choose){
+                case 1:
+                    System.out.println("Wniosek złożony");
+                    break;
+                case 2:
+                    System.out.println("Zmienił się status wniosku");
+                    break;
+                case 3:
+                    System.out.println("wyświetla status wniosku");
+                    break;
+                case 100:
+                    System.out.println("Dziękujemy za korzystanie z programu");
+                    break;
+            }
+
+        }while (choose != 100);
+    }
+}
