585b3959801fcbd1e382f16726faef81d4d833b9
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2021-02-14 14:58:37 +0100

    #42 Zadanie 2 - fabryka napajów - menu dodawania produktów

---
+++ src/polymorphism/Beverage.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class Beverage {
+    private String name;
+    private int capacityInMilliter;
+    private String composition;
+
+    public Beverage(String name, int capacityInMilliter, String composition) {
+        this.name = name;
+        this.capacityInMilliter = capacityInMilliter;
+        this.composition = composition;
+    }
+}
