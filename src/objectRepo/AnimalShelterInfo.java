a1132c19d2ec29edd1e37a5c7d2008cfc8563c40
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2021-02-20 14:13:40 +0100

    #51 Zadanie 2 schronisko -  dodawanie zwierzęcia

---
+++ src/objectRepo/AnimalShelterInfo.java
@@ -0,0 +1,39 @@
+package objectRepo;
+
+public class AnimalShelterInfo {
+    private Animal[] animals = new Animal[20];
+    private int index = 0;
+
+    public void add(Animal animal){
+        if (index < animals.length && !exist(animal)) {
+            animals[index++] = animal;
+
+        }
+    }
+
+    private boolean exist(Animal animal) {
+        for (int i = 0; i < index; i++) {
+            if (animals[i].equals(animal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    private boolean sameChip(Animal animal){
+//        for (int i = 0; i < index; i++) {
+//            if (animals[i].equals(animal)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
+    public void display(){
+        for (int i = 0; i < index; i++){
+            System.out.println(animals[i]);
+            System.out.println("**********************");
+        }
+
+    }
+}
